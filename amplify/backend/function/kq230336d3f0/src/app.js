2f437e1985fd85d77601488b36d3782b8a204cc3
John Doe <csears17@iastate.edu>
2023-03-29 22:04:54 -0500

    i don't know what i'm doing

--- amplify/backend/function/kq230336d3f0/src/app.js
+++ amplify/backend/function/kq230336d3f0/src/app.js
@@ -26,123 +26,64 @@ app.use(function(req, res, next) {
 });
 
 
-var http = require('http');
-var express = require('express');
-var ShareDB = require('sharedb');
-var richText = require('rich-text');
+/**********************
+ * Example get method *
+ **********************/
 
-var WebSocket = require('ws');
-var WebSocketJSONStream = require('@teamwork/websocket-json-stream');
+app.get('/items', function(req, res) {
+  // Add your code here
+  res.json({success: 'get call succeed!', url: req.url});
+});
 
-ShareDB.types.register(richText.type);
-var backend = new ShareDB();
+app.get('/items/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'get call succeed!', url: req.url});
+});
 
+/****************************
+* Example post method *
+****************************/
 
-function start(){
-createDocs(startServer);
+app.post('/items', function(req, res) {
+  // Add your code here
+  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+});
 
-// Create initial document then fire callback
-function createDocs(callback) {
-  var connection = backend.connect();
-  var counter = connection.get('KQ', 'counter');
-  var doc1 = connection.get('KQ', 'richtext1');
-  var doc2 = connection.get('KQ', 'richtext2');
-  var doc3 = connection.get('KQ', 'richtext3');
-  var doc4 = connection.get('KQ', 'richtext4');
-  var doc5 = connection.get('KQ', 'richtext5');
-  var doc6 = connection.get('KQ', 'richtext6');
-  var doc7 = connection.get('KQ', 'richtext7');
-  var doc8 = connection.get('KQ', 'richtext8');
-  var timerDoc = connection.get('KQ', 'timer');
-  
-  doc1.fetch(function(err) {
-    if (err) throw err;
-    if (doc1.type === null) {
-      doc1.create({ops: []}, 'rich-text', function(err) {
-        
-          if (err) throw err;
-        doc2.create({ops: []}, 'rich-text', function(err) {
-          
-          if (err) throw err;
-          doc3.create({ ops: []}, 'rich-text', function(err) {
-            
-            if (err) throw err;
-            doc4.create({ops: []}, 'rich-text', function(err) {
+app.post('/items/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+});
 
-              if (err) throw err;
-              doc5.create({ops: []}, 'rich-text', function(err) {
-                
-                if (err) throw err;
-                doc6.create({ops: []}, 'rich-text', function(err){
-                  
-                  if (err) throw err;
-                  counter.create({numClicks: 0}, function(err){
-                    
-                    if (err) throw err;
-                    doc7.create({ ops: []}, 'rich-text', function(err) {
-                      
-                      if (err) throw err;
-                      doc8.create({ops: []}, 'rich-text', function(err) {
-                        
-                        if (err) throw err;
-                        timerDoc.create({originalTime: 0, remainingTime: 0}, callback);
-      
-                        // Add event listener to update the timer
-                        timerDoc.on('op', function(op, source) {
-                          if (source && source !== connection.id) {
-                            broadcastTimerUpdate(source);
-                          }
-                        });
-                      });
-                    });
-                  });
-                }); 
-              });
-            });
-          });
-        });
-      });
-      return;
-    }
-    callback();
-  });
-}
+/****************************
+* Example put method *
+****************************/
 
-function startServer() {
-  // Create a web server to serve files and listen to WebSocket connections
-  app.use(express.static('static'));
-  app.use(express.static('node_modules/quill/dist'));
-  var server = http.createServer(app);
+app.put('/items', function(req, res) {
+  // Add your code here
+  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+});
 
-  // Connect any incoming WebSocket connection to ShareDB
-  var wss = new WebSocket.Server({
-    server: server,
-    // Set the WebSocket server to use a secure connection
-    verifyClient: function (info, cb) {
-      // Check if the request is secure
-      if (info.secure) {
-        cb(true);
-      } else {
-        cb(false, 400, 'Secure connection required');
-      }
-    }
-  });
+app.put('/items/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+});
 
-  wss.on('connection', function(ws) {
-    var stream = new WebSocketJSONStream(ws);
-    backend.listen(stream);
-  });
+/****************************
+* Example delete method *
+****************************/
 
-  // Serve the editor HTML page
-  app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/editor.html');
-  });
-}
-}
+app.delete('/items', function(req, res) {
+  // Add your code here
+  res.json({success: 'delete call succeed!', url: req.url});
+});
+
+app.delete('/items/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'delete call succeed!', url: req.url});
+});
 
 app.listen(3000, function() {
-    start();
-    console.log("App started");
+    console.log("App started")
 });
 
 // Export the app object. When executing the application local this does nothing. However,
